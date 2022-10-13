c3b064946f9e85f0ac81d55eb201dc082cb2b38e
flashruler <44584657+flashruler@users.noreply.github.com>
2022-10-13 13:01:12 -0700

    nyooom

---
+++ ListTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+    static void testFilter(int[] arr) {
+        int temp;
+        for(int i = 0; i < arr.length/2; i++) {
+          temp=arr[i];
+          arr[i]=arr[arr.length-i-1];
+          arr[arr.length-i-1]=temp;
+        }
+      }
+}
